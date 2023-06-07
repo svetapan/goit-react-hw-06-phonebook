0cf620171e4680cb7e96686b8f75917590a1c35b
Svetlana Panenko <svetabagira1986@gmail.com>
2023-06-07 21:01:16 +0300

    deleted actions

--- src/store/actions.js
+++
@@ -1,6 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-export const addContact = createAction('ADDCONTACT');
-export const deleteContact = createAction('DELETECONTACT');
-export const updateContact = createAction('UPDATECONTACT');
-export const filterContact = createAction('FILTERCONTACT');
